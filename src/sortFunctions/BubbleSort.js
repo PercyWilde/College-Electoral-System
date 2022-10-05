40f89a557e55b4b60e8f4ff9bf1636f90e7d2c99
PercyWilde <67506583+PercyWilde@users.noreply.github.com>
2022-10-05 20:16:20 +0530

    Update BubbleSort.js

--- src/sortFunctions/BubbleSort.js
+++ src/sortFunctions/BubbleSort.js
@@ -1,6 +1,6 @@
 export async function* BubbleSort(array, swap, highlight, marksort) {
   for (let i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length - i - 1; j++) {
+    for (var j = 1; j < array.length - i - 1; j++) {
       yield await highlight([j, j + 1]);
 
       if (array[j] > array[j + 1]) {
